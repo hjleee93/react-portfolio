2cb1a73a98a54104676b64e202e3d934c3363873
hyeonjeong <hj.lee@fromthered.com>
2022-12-13 23:35:10 +0900

    1. 프로젝트 디자인수정 2. 푸터 링크 연결

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,27 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import WebAssetIcon from '@mui/icons-material/WebAsset';
+import Tooltip from '@mui/material/Tooltip';
 function Footer() {
 return ( 
   <div style={{backgroundColor:"#444444", height:'200px', display:'flex',
     flexDirection:'column'
   }}> 
   <div style={{paddingTop:'50px'}}>
-    <GitHubIcon style={{marginRight:'20px', color:'#fff'}}/>
-    <WebAssetIcon style={{color:'#fff'}}/>
+    
+  <Tooltip title="Github">
+    <GitHubIcon
+      onClick={() => window.open(process.env.REACT_APP_GIT_HUB)}
+      style={{marginRight:'20px', color:'#fff', cursor:'pointer'}} 
+      />
+    </Tooltip>
+
+    <Tooltip title="Blog">
+      <WebAssetIcon 
+        onClick={() => window.open(process.env.REACT_APP_BLOG)}
+        style={{color:'#fff', cursor:'pointer'}}/>
+    </Tooltip>
   </div>     
-  <p style={{color:'#fff'}}>Copyright 2022. Hyeonjeong</p>
+  <p style={{color:'#fff'}}> Copyright 2022. Hyeonjeong</p>
  
   </div>
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,90 +1,161 @@
+import { useState } from 'react';
+import '../common.css'
 import styled from 'styled-components';
 import ts from '../assets/ts.png'
 import vue from '../assets/vue.png'
 import js from '../assets/js.png'
 import nuxt from '../assets/nuxt.png'
-import { Button } from '@mui/material';
+import zempie from '../assets/zempie.svg'
+import miliverse from '../assets/miliverse.png'
+import kia from '../assets/kia.webp'
+
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CheckIcon from '@mui/icons-material/Check';
+
 
 const img: any = {
   ts,
   vue,
   js,
-  nuxt
+  nuxt,
+  zempie,
+  miliverse,
+  kia
 }
 
+
 function Projects() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const projects = [
     {
       title:'Zempie',
       langs:['nuxt', 'vue', 'ts'],
       desc1:'HTML5 게임을 공유하는 SNS 형식의 웹 어플리케이션',
-      desc2:'Nuxt.js 및 Vue.js 프론트엔드 개발, Node.js 백엔드 유지보수',
+      desc2:`Nuxt.js 와 Vue.js 프론트 개발을 진행했으며 Node.js와 sequelize를 사용하여 백엔드 유지보수를 진행했습니다.
+      어드민페이지는 Quasar 프레임워크를 사용하여 개발을 진행했습니다.`,
       period:'2021.01~2022.12(2년)',
       infos:[
         'Vue.js 2.0 에서 Nuxt.js로 전환 후 SEO 적합성 100 달성 완료 후 google analytics 방문자 수 30% 증가',
         'Google Analytics 4 와 BigQuery를 이용하여 단순 테이블에서 Chart.js를 이용하여 가시성을 50%이상 향상',
         'Quasar 프레임워크를 이용하여 어드민페이지 개발을 빠르게 진행',
-      ]
+      ],
+      img:'zempie',
+      color:'#ff6e17',
      
     },
     {
       title:'Miliverse',
       langs:['vue', 'js'],
       desc1:'전세계 맵을 10m²로 분할하여 부동산 메타버스를 제공하는 웹 어플리케이션',
-      desc2:'Vue.js 프론트엔드 개발',
+      desc2:'Vue.js의 Quasar프레임워크를 이용하여 프론트엔드 개발을 진행했습니다.',
       period:'2022.01~2022.07(7개월)',
       infos:[
         'Mapbox API를 사용하여 가상 부동산 메타버스 구현, 전체적인 맵의 비즈니스 로직 담당.',
         'Vue.js 3.0을 Composition Api를 도입하여 프로젝트 생산성을 10%높임',
         'Firebase 기반 로그인으로 카카오톡, 구글, 페이스북 로그인을 사용 사용자의 편의성을 높임', 
-      ]
+      ],
+      img:'miliverse',
+      color:'#241712'
     },
     {
-      title:'Kia sns',
+      title:'KIA SNS',
       langs:['vue', 'ts'],
       desc1:'기아 사내 SNS 웹 어플리케이션',
       desc2:'Vue.js 프론트엔드 개발',
       period:'2021.04~2021.08 (5개월)',
-      infos:[]
+      infos:[],
+      img:'kia',
+      color:'transparent'
     },
     
   ]
   
-  const Box = styled.div`
+  const Container = styled.div`
     margin: 40px;  
+    box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.05);
+    padding:20px;
+    display:flex;
   `;
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
   const cards = projects.map((project)=>{
+   
+
     const lan =  project.langs.map((lan)=>{
       return <img src={img[lan]} width={30} key={lan}/>
     })
     const infos = project.infos.map((info) =>{
-      return <li>{info}</li>
+      return <li style={{display:'flex'}}><CheckIcon style={{paddingTop:'3px'}}/>{info}</li>
      
     })
 
     return (
       <div>
-        <Box key={project.title} className='project-card'>
-          <div>
-            <p><strong>{project.title}</strong></p>
-            <small>{project.period}</small>
+        <Container key={project.title} className='project-card'>
+          <div style={{ backgroundColor:project.color, borderRadius:'10px', padding:'30px'}}>
+            <img src={img[project.img]} style={{width:'100%'}} />
           </div>
+          <div>
+            <h2><strong>{project.title}</strong></h2>
+            <small>{project.period}</small>
           <p>
-            {project.desc1}
+            <b>{project.desc1}</b>
           </p>
           <p>{project.desc2}</p>
-            <p>Main Skills : {
-              <span>{lan}</span>
+            <p style={{display: 'flex', justifyContent: 'center'}}>Made with {
+              <span style={{marginLeft:'10px'}}>{lan}</span>
               }
             </p>
             <ul>
               {infos}
             </ul>
         
-        </Box>
-        <Button>주요 코드 확인하기</Button>
+          <Button onClick={handleOpen}>주요 코드 확인하기</Button>
+
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+
+        </div>
+        </Container>
       </div>
+      
 
     )
   })
@@ -92,7 +163,7 @@ function Projects() {
 
 return ( 
 <div>
-  <h3>Projects</h3>
+  <h1 style={{height:'100px', display:'flex', alignItems: 'center',justifyContent: 'center'}}>PROJECTS</h1>
   {cards}
 </div>
      )
